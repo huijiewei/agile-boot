7676abb33148c23b55c51f3c4ff0dd0dcf31abc2
Huijie Wei <huijiewei@outlook.com>
2021-03-04 14:59:07 +0800

    更新

---
+++ agile-serve-restful/src/main/java/com/huijiewei/agile/serve/restful/RestfulServerApplication.java
@@ -0,0 +1,33 @@
+package com.huijiewei.agile.serve.restful;
+
+import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
+
+/**
+ * @author huijiewei
+ */
+@EnableCaching
+@SpringBootApplication(exclude = {
+        ErrorMvcAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
+@ComponentScan(basePackages = "com.huijiewei.agile")
+@EntityScan(basePackages = "com.huijiewei.agile")
+@EnableJpaRepositories(
+        repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class,
+        basePackages = "com.huijiewei.agile",
+        bootstrapMode = BootstrapMode.DEFERRED
+)
+public class RestfulServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulServerApplication.class, args);
+    }
+}
---
+++ agile-serve-restful/src/main/java/com/huijiewei/agile/serve/restful/handler/ExceptionHandling.java
@@ -0,0 +1,65 @@
+package com.huijiewei.agile.serve.restful.handler;
+
+import com.huijiewei.agile.core.exception.BadRequestException;
+import com.huijiewei.agile.core.exception.ConflictException;
+import com.huijiewei.agile.core.exception.ForbiddenException;
+import com.huijiewei.agile.core.exception.NotFoundException;
+import org.apiguardian.api.API;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+import org.zalando.problem.StatusType;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+import static org.apiguardian.api.API.Status.INTERNAL;
+import static org.zalando.problem.Status.UNPROCESSABLE_ENTITY;
+
+/**
+ * @author huijiewei
+ */
+
+@API(status = INTERNAL)
+@ControllerAdvice
+public class ExceptionHandling implements ProblemHandling, SecurityAdviceTrait {
+    @Override
+    public StatusType defaultConstraintViolationStatus() {
+        return UNPROCESSABLE_ENTITY;
+    }
+
+    @Override
+    public boolean isCausalChainsEnabled() {
+        return false;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleNotFound(
+            final NotFoundException exception,
+            final NativeWebRequest request) {
+        return create(Status.NOT_FOUND, exception, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleBadRequest(
+            final BadRequestException exception,
+            final NativeWebRequest request) {
+        return create(Status.BAD_REQUEST, exception, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleForbidden(
+            final ForbiddenException exception,
+            final NativeWebRequest request) {
+        return create(Status.FORBIDDEN, exception, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleConflict(
+            final ConflictException exception,
+            final NativeWebRequest request) {
+        return create(Status.CONFLICT, exception, request);
+    }
+}
